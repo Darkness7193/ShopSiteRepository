cc4fe8d37e1a8ea198f840091e13bbad3ddf9abd
27_07 <27_07b@mail.ru>
2023-05-13 02:16:06 +0300

    Version 0.9

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import create, delete, show, update, crud
+from app.views import crud
 
 urlpatterns = [
-    path('create/', create, name='create'),
-    path('delete/', delete, name='delete'),
-    path('show/', show, name='show'),
-    path('update/', update, name='update'),
     path('crud/', crud, name='crud'),
 ]
--- app/views.py
+++ app/views.py
@@ -7,32 +7,6 @@ def index(request):
     return render(request, 'app/index.html', context)
 
 
-def create(request):
-    context = {
-
-    }
-    return render(request, 'app/create.html', context)
-
-
-def delete(request):
-    context = {
-    }
-    return render(request, 'app/delete.html', context)
-
-
-def show(request):
-    context = {
-        'products': Product.objects.all(),
-    }
-    return render(request, 'app/show.html', context)
-
-
-def update(request):
-    context = {
-    }
-    return render(request, 'app/update.html', context)
-
-
 def crud(request):
     context = {
         'products': Product.objects.all()
