9e409cf8c0d18442ebd314090e319830e9da6852
27_07 <27_07b@mail.ru>
2023-05-16 19:54:44 +0300

    Version 0.18.2

--- app/static/app/js/delete.js
+++ app/static/app/js/delete.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
     $(document).on("click", ".delete-btn", function(){
         let tr = $(this).parents("tr");
         $().click(ajaxDelete(tr));
-
         $(".create-new").removeAttr("disabled");
         tr.remove();
     });
