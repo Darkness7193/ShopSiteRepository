cdaf931a427c672615be8d2017d382a85ce38d28
27_07 <27_07b@mail.ru>
2023-05-18 01:15:41 +0300

    Version 0.32.1

--- ProfilesApp/validations.py
+++ ProfilesApp/validations.py
@@ -6,49 +6,40 @@ from django.db.models import Q
 
 def sign_in_validation(request, username, password, password_conf):
     is_login_exist = get_or_none(User, username=username)
+    error = False
 
-    errors = []
     if password != password_conf:
-        errors.append('Введенные пароли не совпадают')
+        messages.error(request, 'Введенные пароли не совпадают')
+        error = True
     if is_login_exist:
-        errors.append('Введенный логин уже используется')
+        messages.error(request, 'Введенный логин уже используется')
+        error = True
 
-    if errors:
-        for error_content in errors:
-            messages.error(request, error_content)
-
-    return request, errors != []
+    return request, error
 
 
 def password_change_validation(request, old_password, password, password_conf):
     user = request.user
-    errors = []
+    error = False
 
     if password != password_conf:
-        errors.append('Введенные пароли не совпадают')
+        messages.error(request, 'Введенные пароли не совпадают')
     if not user.check_password(old_password):
-        errors.append('Введенный пароль не совпадает с паролем аккаунта')
+        messages.error(request, 'Введенный пароль не совпадает с паролем аккаунта')
 
-    if errors:
-        for error_content in errors:
-            messages.error(request, error_content)
-
-    return request, errors != []
+    return request, error
 
 
 def username_change_validation(request, new_username, password):
     user = request.user
+    is_login_exist = bool(User.objects.filter(~Q(id=user.id), username=new_username))
+    error = False
 
-    is_login_exist = User.objects.filter(~Q(id=user.id), username=new_username) == []
-
-    errors = []
     if is_login_exist:
-        errors.append('Введенный логин уже используется')
+        messages.error(request, 'Введенный логин уже используется')
+        error = True
     if not user.check_password(password):
-        errors.append('Введенный пароль не совпадает с паролем аккаунта')
+        messages.error(request, 'Введенный пароль не совпадает с паролем аккаунта')
+        error = True
 
-    if errors:
-        for error_content in errors:
-            messages.error(request, error_content)
-
-    return request, errors != []
+    return request, error
