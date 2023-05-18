07884cc7d8161f6a9c339a7434127460ca189766
27_07 <27_07b@mail.ru>
2023-05-18 03:50:26 +0300

    Version 0.35 I remove this fucking bootstrap-min

--- app/static/app/js/create-new.js
+++ app/static/app/js/create-new.js
@@ -4,9 +4,9 @@ let input_tr = `
 	<td><input type="text" class="form-control" name="description-input" id="description-input"></td>
 	<td><input type="text" class="form-control" name="count-input" id="count-input"></td>
 	<td>
-	    <a class="create-btn" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-	    <a class="update-btn" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-	    <a class="delete-btn" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
+	    <a class="create-btn"><i class="material-icons">&#xE03B;</i></a>
+	    <a class="update-btn"><i class="material-icons">&#xE254;</i></a>
+	    <a class="delete-btn"><i class="material-icons">&#xE872;</i></a>
 	</td>
 `;
 
